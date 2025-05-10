faf8d6e61fcdfc9948b1595d52cd17f51ba59f3c
Chris Romp <ChrisRomp@users.noreply.github.com>
2025-05-09 21:47:55 -0700

    Add ESLint directive to test-utils for component export compliance
    - Added an ESLint directive to suppress warnings related to exporting components from the test-utils file.
    - Ensured compatibility with React Refresh for better development experience.

--- apps/web/src/utils/test-utils.tsx
+++ apps/web/src/utils/test-utils.tsx
@@ -16,4 +16,5 @@ export function renderWithRouter(
 }
 
 // Re-export everything from @testing-library/react
+// eslint-disable-next-line react-refresh/only-export-components
 export * from '@testing-library/react'; 
