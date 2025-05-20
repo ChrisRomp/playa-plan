64fcab18ea6fb92ce1cd0c67d3f8ac1e4e214a1d
Chris Romp <ChrisRomp@users.noreply.github.com>
2025-05-20 16:06:19 -0700

    fix(web): Update ConfigContext import path in UserDashboard.test.tsx

--- apps/web/src/components/home/UserDashboard.test.tsx
+++ apps/web/src/components/home/UserDashboard.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import UserDashboard from './UserDashboard';
 import { AuthContext } from '../../store/authUtils';
-import { ConfigContext } from '../../store/ConfigContext';
+import { ConfigContext } from '../../store/ConfigContextDefinition';
 import { User, CampConfig } from '../../types';
 
 describe('UserDashboard', () => {
